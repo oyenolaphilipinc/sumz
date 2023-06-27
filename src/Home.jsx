bd103ac1da1e1ca93a3b2c354a0eaf223978aef2
oyefeso <oyefesojoshua@gmail.com>
2023-06-26 23:21:25 -1100

    modified:   src/Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,12 @@
 import Hero from "./components/Hero";
 import Demo from "./components/Demo";
-import Account from "./components/Account";
+
 import "./App.css";
 
 const Home = () => {
   return (
     <main>
-      {/* <Account /> */}
+     
       
       <div className='main'>
         <div className='gradient' />
