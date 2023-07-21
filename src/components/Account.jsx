38b66ee38e2dcb6c65c0d2ff99a933a9d26dc6f6
Oyefes <98193829+Oyefes@users.noreply.github.com>
2023-07-21 21:29:21 +0100

    Update Account.jsx

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -157,9 +157,9 @@ export default function Account() {
     <div className="borderLine2"></div>
         <Card className="
         h-[509px]
-         translate-y-[-48px]
+         translate-y-[-45px]
         rounded-tl-[15px] mt-[50px]  
-        bg-[#23242a] mx-[4px]
+        bg-[#23242a] mx-[2px]
         rounded-tr-[15px]">
             <h1 className="text-center sm:text-[15px] text-[15px] md-[15px]  xl:text-[15px] lg-[15px] transition duration-[0.5s] ease-in  text-white-700 font-bold">Summarize easily with the power of AI</h1>
             <img src="https://app.dub.sh/_static/illustrations/shopping-call.svg" height={350} width={370} className="mx-auto"/>
